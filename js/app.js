ec0b0331c9e84dcd2cc501634f0624eb443b4923
leowolf749 <leowolf749@gmail.com>
2017-01-25 22:58:19 -0500

    Styled the page more. Can get the ratings to work but only on 1 list item.

--- js/app.js
+++ js/app.js
@@ -7,11 +7,23 @@ app.controller('MovieList', function ($scope, MovieService){
     $scope.movies = MovieService.getMovies();
 
     $scope.newMovie = function () {
-        MovieService.addMovie($scope.movieTitle, $scope.movieDirector, $scope.movieRelease, $scope.movieGenre);
+        MovieService.addMovie($scope.movieTitle, $scope.movieDirector, $scope.movieRelease, $scope.movieGenre, $scope.isGood);
         $scope.movieTitle = '';
         $scope.movieDirector = '';
         $scope.movieRelease = '';
         $scope.movieGenre = '';
+        $scope.isGood = null;
+
+    }
+
+    $scope.fresh = function () {
+        $scope.isGood = true;
+        
+    }
+
+    $scope.rotten = function () {
+        $scope.isGood = false;
+        
     }
     
 });
@@ -20,12 +32,14 @@ app.factory('MovieService', function () {
     let movies = [];
 
     return {
-        addMovie: function (title, director, release, genre) {
+        addMovie: function (title, director, release, genre, isGood) {
             movies.push({
                 title: title,
                 director: director,
                 release: release,
                 genre: genre,
+                isGood: null,
+    
             });
         },
         getMovies: function () {
--- public/app.js
+++ public/app.js
@@ -8,11 +8,23 @@ app.controller('MovieList', function ($scope, MovieService){
     $scope.movies = MovieService.getMovies();
 
     $scope.newMovie = function () {
-        MovieService.addMovie($scope.movieTitle, $scope.movieDirector, $scope.movieRelease, $scope.movieGenre);
+        MovieService.addMovie($scope.movieTitle, $scope.movieDirector, $scope.movieRelease, $scope.movieGenre, $scope.isGood);
         $scope.movieTitle = '';
         $scope.movieDirector = '';
         $scope.movieRelease = '';
         $scope.movieGenre = '';
+        $scope.isGood = null;
+
+    }
+
+    $scope.fresh = function () {
+        $scope.isGood = true;
+        
+    }
+
+    $scope.rotten = function () {
+        $scope.isGood = false;
+        
     }
     
 });
@@ -21,12 +33,14 @@ app.factory('MovieService', function () {
     let movies = [];
 
     return {
-        addMovie: function (title, director, release, genre) {
+        addMovie: function (title, director, release, genre, isGood) {
             movies.push({
                 title: title,
                 director: director,
                 release: release,
                 genre: genre,
+                isGood: null,
+    
             });
         },
         getMovies: function () {
